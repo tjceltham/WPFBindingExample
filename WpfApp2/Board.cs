3fc1da6dc0f6fb18f1783ef772307e6aae84f4db
Tony Collins <tjc@eltham-college.org.uk>
2020-10-29 14:34:51 +0000

    Add project files.

---
+++ WpfApp2/Board.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WpfApp2
+{
+    class Board
+    {
+        private int colour = 0;
+
+        public void setColour(int c)
+        {
+            colour = c;
+        }
+    }
+}
---
+++ WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Board b;
+        public MainWindow()
+        {
+            InitializeComponent();
+            b = new Board();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string r = sender.ToString();
+            
+            if (r== "System.Windows.Controls.Button: White")
+            {
+                b.setColour(0);
+            }
+            if(r== "System.Windows.Controls.Button: Red")
+            {
+                b.setColour(1);
+            }
+            if (r == "System.Windows.Controls.Button: Yellow")
+            {
+                b.setColour(2);
+            }
+        }
+    }
+}
